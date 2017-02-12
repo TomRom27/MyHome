d98d2f81e4bae2bb5e4001eb083b3af656b82682
tomrom27 <tomek.romanowski@gmail.com>
2017-02-12 19:35:24 +0100

    API configured to return Json, azure web deploy added

--- MyHome/App_Start/WebApiConfig.cs
+++ MyHome/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers;
 
 namespace MyHome
 {
@@ -10,6 +11,7 @@ namespace MyHome
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
